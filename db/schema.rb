053f21c5e019e8141b9f051eccf9d52872b155af
pilarblancoraynal <pilarblancoraynal@gmail.com>
2019-02-22 01:24:13 +0100

    agg. collonna utenti ai contenuti

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190221174531) do
+ActiveRecord::Schema.define(version: 20190222001328) do
 
   create_table "contents", force: :cascade do |t|
     t.string "titolo"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20190221174531) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
