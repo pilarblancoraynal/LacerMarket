0d2f968b9c6691059e1fe42894002606bf8b25b4
pilarblancoraynal <pilarblancoraynal@gmail.com>
2019-03-26 16:06:37 +0100

    tocchi finali...ancora da sistemare prob. jquery dashboard e fixed navbar e i nomi dello storico nella dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action :store_location
 
   protected
 
@@ -8,4 +12,21 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
+
+  def store_location
+  # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        request.fullpath != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+
+      session[:previous_url] = request.fullpath
+   end
+ end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
 end
